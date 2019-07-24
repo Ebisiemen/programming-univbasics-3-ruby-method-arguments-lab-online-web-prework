e50a0ccda2c3c6237b2169db8e4424d8a44f3d24
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-07-24 15:01:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,8 @@ end
 def introduction_with_language_optional (name)
    puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end  
-  
+ 
+def introduction_with_language_optional (name, language)
+   puts "Hi, my name is #{name} and I am learning to program in Ruby."
+end   
   
